51b55fc980d263bb4dbd23ea1f20591f67ab209f
Adnan Smajlovic <asmajlovic@marsdd.com>
2022-04-13 23:22:35 -0400

    feat: :art: v0.1 ta-daa

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let symbol = 'spade';
+  export let color = 'red';
+  export let letter = 'A';
+  export let size = 'top'; // full, top
+
+  const colors = {
+    red: 'text-red-800',
+    black: 'text-black-800',
+  };
+
+  // a.s. reference: https://www.compart.com/en/unicode/block/U+2500
+  const topLeftCorner = String.fromCharCode(0x2554);
+  const topRightCorner = String.fromCharCode(0x2557);
+  const bottomLeftCorner = String.fromCharCode(0x255a);
+  const bottomRightCorner = String.fromCharCode(0x255d);
+  const doubleVertical = String.fromCharCode(0x2551);
+  const doubleHorizontal = String.fromCharCode(0x2550);
+  const enSpace = String.fromCharCode(0x2003);
+
+  // ♠ ♣ ♥ ♦
+  const symbols = {
+    spade: String.fromCharCode(0x2660),
+    club: String.fromCharCode(0x2663),
+    heart: String.fromCharCode(0x2665),
+    diamond: String.fromCharCode(0x2666),
+  };
+
+  // ╔═════╗
+  // ║     ║
+  // ║     ║
+  // ║     ║
+  // ╚═════╝
+</script>
+
+<div class="m-0 font-mono text-green-900">
+  <div>
+    {topLeftCorner}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{topRightCorner}
+  </div>
+  <div>
+    {doubleVertical}{letter.length > 1
+      ? ''
+      : enSpace}{letter}{enSpace}{enSpace}{enSpace}{enSpace}<span
+      class={colors[color]}>{symbols[symbol]}</span
+    >{doubleVertical}
+  </div>
+  {#if size === 'full'}
+    {#each { length: 3 } as _, i}
+      <div>
+        {doubleVertical}{enSpace}{enSpace}{enSpace}{enSpace}{enSpace}{enSpace}{enSpace}{doubleVertical}
+      </div>
+    {/each}
+
+    <div>
+      {bottomLeftCorner}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{doubleHorizontal}{bottomRightCorner}
+    </div>
+  {/if}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Card from '../lib/components/Card.svelte';
+
+  const arr = [];
+
+  const letters = [
+    'A',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
+
+  const symbols = ['spade', 'club', 'heart', 'diamond'];
+
+  letters.sort(() => Math.random() - 0.5);
+  symbols.sort(() => Math.random() - 0.5);
+
+  for (const letter of letters) {
+    for (const symbol of symbols) {
+      arr.push({
+        color: ['heart', 'diamond'].includes(symbol) ? 'red' : 'black',
+        letter,
+        symbol,
+      });
+    }
+  }
+
+  // shuffle array
+  arr.sort(() => Math.random() - 0.5);
+
+  console.log(arr);
+
+  const top10Cards = arr.slice(0, 10);
+  const restOfCards = arr.slice(10);
+
+  top10Cards.sort(() => Math.random() - 0.5);
+  restOfCards.sort(() => Math.random() - 0.5);
+</script>
+
+<h1 class="text-3xl font-semibold items-center text-center m-2">
+  Pasians - Solitaire
+</h1>
+
+<div class="grid grid-cols-10 gap-1 ml-2 mb-4">
+  {#each top10Cards as { color, letter, symbol }, i}
+    <Card size="full" {letter} {symbol} {color} />
+  {/each}
+</div>
+
+<div class="grid grid-cols-7 gap-0 ml-2 mb-4">
+  {#each restOfCards as { color, letter, symbol }, i}
+    <Card size={i > 34 ? 'full' : 'top'} {letter} {symbol} {color} />
+  {/each}
+</div>
